020fd6f1ff76d23b1855eff989c546a43669bf3f
Platov Sergei <serg1764@gmail.com>
2023-10-23 18:35:53 +0300

    lesson - 38 OS и path

---
+++ services/storage.service.js
@@ -0,0 +1,9 @@
+import { homedir } from 'os';
+import { join/*, basename, dirname, extname, relative, isAbsolute, resolve, sep */} from 'path';
+
+const filwPath = join('', 'D:\\study\\Node\\weather-cli\\weather-data.json');
+const saveKeyValue = (key, value) => {
+    console.log();
+};
+
+export { saveKeyValue }
