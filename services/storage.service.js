6380304576a07cd4ce6d67af9a9fca1297a336b0
Platov Sergei <serg1764@gmail.com>
2023-10-23 19:06:55 +0300

    lesson - 39 Работа с файловой системой

--- services/storage.service.js
+++ services/storage.service.js
@@ -1,9 +1,37 @@
 import { homedir } from 'os';
 import { join/*, basename, dirname, extname, relative, isAbsolute, resolve, sep */} from 'path';
+import { promises } from 'fs';
 
-const filwPath = join('', 'D:\\study\\Node\\weather-cli\\weather-data.json');
-const saveKeyValue = (key, value) => {
-    console.log();
+const filePath = join('', 'D:\\study\\Node\\weather-cli\\weather-data.json');
+const saveKeyValue = async (key, value) => {
+    let data = {};
+
+    if (await isExist(filePath)){
+        const file = await promises.readFile(filePath);
+        data = JSON.parse(file);
+    }
+
+    data[key] = value;
+    await promises.writeFile(filePath, JSON.stringify(data));
 };
 
-export { saveKeyValue }
+const getKeyValue = async (key)=> {
+    if (await isExist(filePath)){
+        const file = await promises.readFile(filePath);
+        const data = JSON.parse(file);
+        return data[key];
+    }
+    return undefined;
+}
+
+const isExist = async (path) => {
+    try{
+        await promises.stat(path);
+        return true;
+    }
+    catch (e) {
+        return false;
+    }
+}
+
+export { saveKeyValue, getKeyValue }
--- weather.js
+++ weather.js
@@ -1,7 +1,22 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import {printHelp} from "./services/log.service.js";
+import {printHelp, printSuccess, printError} from "./services/log.service.js";
 import {saveKeyValue} from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+    if(!token.length){
+        printError('Не передан токен');
+        return;
+    }
+
+    try {
+        await saveKeyValue('token', token);
+        printSuccess('Токен Сохранен');
+    }
+    catch (e) {
+        printError(e.message);
+    }
+}
 const initCLI = () => {
     const args = getArgs(process.argv);
     if(args.h){
@@ -12,7 +27,7 @@ const initCLI = () => {
     }
     if(args.t){
         //Сохранить токен
-        saveKeyValue('token', args.t);
+        return saveToken(args.t);
     }
 }
 
