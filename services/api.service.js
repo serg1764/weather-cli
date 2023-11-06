6c246b17bacbc93e7fd27cb792faf65e391db17b
Platov Sergei <serg1764@gmail.com>
2023-11-06 18:57:35 +0300

    lesson - 41 Переменные окружения

--- services/api.service.js
+++ services/api.service.js
@@ -3,12 +3,13 @@ import { getKeyValue, TOKEN_DICTIONARY } from "./storage.service.js";
 import axios from "axios";
 
 const getWeather = async (city) => {
-    const token = await getKeyValue(TOKEN_DICTIONARY.token);
+    // задать env в windows можно так в командной строке $env:TOKEN = "f3054af964dee8ab0ccdb3879103d27b"
+    const token = process.env.TOKEN ?? await getKeyValue(TOKEN_DICTIONARY.token);
     if(!token){
         throw new Error('Не задан ключ API, задайте его через команду -t [API_KEY]');
     }
 
-    const { data } = await axios.get('https://api.openweathermap.org/geo/1.0/direct',{
+    let { data } = await axios.get('https://api.openweathermap.org/geo/1.0/direct',{
         params : {
             'q': city,
             'limit': '1',
@@ -18,8 +19,31 @@ const getWeather = async (city) => {
         }
     });
 
-    //console.log(data);
-    return data;
+//https://openweathermap.org/current тут бесплатное апи
+    //https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}
+    axios.get('https://api.openweathermap.org/data/2.5/weather',{
+        params : {
+            'lat': data[0].lat,
+            'lon': data[0].lon,
+            'appid': token,
+            'exclude': 'current',
+            'lang': 'ru',
+            'units': 'metrics'
+        }
+    })
+    .then((response) => {
+        const data_city = response.data;
+        console.log(data);
+        console.log(data[0].lat);
+        console.log(data[0].lon);
+        console.log(data_city);
+        // Здесь вы можете продолжить работу с data_city
+        return data_city;
+    })
+    .catch((error) => {
+        console.error('Произошла ошибка при выполнении второго запроса:', error);
+    });
+    //return data_city;
 
     //const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${key}`;
     /*const url = new URL ('https://api.openweathermap.org/geo/1.0/direct');
--- weather.js
+++ weather.js
@@ -20,6 +20,7 @@ const saveToken = async (token) => {
 }
 const initCLI = () => {
     const args = getArgs(process.argv);
+    //console.log(process.env);
     if(args.h){
         printHelp();
     }
